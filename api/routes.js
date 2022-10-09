f554703ca32719a45058717da35ea6a47c57ebbf
Lynh <chinsugaming@gmail.com>
2022-10-09 22:43:21 +0700

    bai tap

--- api/routes.js
+++ api/routes.js
@@ -1,13 +1,21 @@
 "use strict";
 module.exports = function (app) {
-  let productsCtrl = require("./controllers/ProductsController");
+  // let productsCtrl = require("./controllers/ProductsController");
+  let drinksCtrl = require("./controllers/Drinks.js");
 
   // todoList Routes
-  app.route("/products").get(productsCtrl.get).post(productsCtrl.store);
+  // app.route("/products").get(productsCtrl.get).post(productsCtrl.store);
+  app.route("/Drinks").get(drinksCtrl.get).post(drinksCtrl.store);
 
   app
-    .route("/products/:productId")
-    .get(productsCtrl.detail)
-    .put(productsCtrl.update)
-    .delete(productsCtrl.delete);
+    //products
+    // .route("/products/:productId")
+    // .get(productsCtrl.detail)
+    // .put(productsCtrl.update)
+    // .delete(productsCtrl.delete)
+    //drinks
+    .route("/Drinks/:drinkId")
+    .get(drinksCtrl.detail)
+    .put(drinksCtrl.update)
+    .delete(drinksCtrl.delete);
 };
